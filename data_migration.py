8d19bd63c1f459c9215b068aac139c6530266a8d
Ntp <ntpshr.b@gmail.com>
2022-10-09 14:11:36 +0700

    update visualize func.

--- data_migration.py
+++ data_migration.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import csv, sqlite3
 import argparse
 from datetime import datetime
+import numpy as np
     
 
 field_name = ['EMPID', 'PASSPORT', 'FIRSTNAME', 'LASTNAME', 'GENDER', 'BIRTHDAY', 'NATIONALITY', 'HIRED', 'DEPT', 'POSITION', 'STATUS', 'REGION']
@@ -19,7 +20,7 @@ def xml2csv(xml_file, csv_file):
         writer.writeheader()
         writer.writerows(data_lst)
 
-def cal_work_exp(date):
+def cal_age(date):
     today = date.today()
     work_exp = today.year - date.year - ((today.month, today.day) < (date.month, date.day))
     return work_exp
@@ -34,7 +35,7 @@ def clean_data(records):
             record.find('STATUS').text == '1' and\
             record.find('EMPID').text not in existed_empid and\
             record.find('PASSPORT').text not in existed_passport and\
-            cal_work_exp(datetime.strptime(record.find('HIRED').text, '%d-%m-%Y').date()) > 3:
+            cal_age(datetime.strptime(record.find('HIRED').text, '%d-%m-%Y').date()) > 3:
 
             data_return.append({el.tag: el.text for el in record})
             existed_empid.append(record.find('EMPID').text)
@@ -65,16 +66,33 @@ def insert2sqlite(csv_file, database_name, table_name):
 
 def visualize(csv_file):
     df = pd.read_csv(csv_file)
-    # pie chart: gender
-    plt.subplot(121)
+    figure, axis = plt.subplots(2, 2)
     data = [len(df[df['GENDER']==0]), len(df[df['GENDER']==1])]
-    plt.pie(data, labels = ["Male", "Female"])
-    plt.xlabel("Gender")
+    axis[0, 0].pie(data, labels = ["Male", "Female"])
+    axis[0, 0].set_title("Gender")
 
-    plt.subplot(122)
     region = df['REGION'].value_counts().rename_axis('region').reset_index(name='counts')
-    plt.pie(region['counts'], labels=region['region'])
-    plt.xlabel("Region")
+    axis[0, 1].pie(region['counts'], labels=region['region'])
+    axis[0, 1].set_title("Region")
+
+    df['WORK_EXP'] = df.apply(lambda x: cal_age(datetime.strptime(x['HIRED'], '%d-%m-%Y').date()), axis=1)
+    axis[1, 0].barh(df['FIRSTNAME'], df['WORK_EXP'], color ='maroon')
+    axis[1, 0].invert_yaxis()
+    axis[1, 0].set_xlabel('Years')
+    axis[1, 0].set_title('Work Experience')
+
+    df_gender_count = df.groupby(["GENDER", "POSITION"]).size().reset_index()
+    df_male = df_gender_count[df_gender_count['GENDER'] == 0]
+    df_female = df_gender_count[df_gender_count['GENDER'] == 1]
+    ind = np.arange(3)
+    width = 0.35
+    p1 = axis[1, 1].bar(ind, tuple(df_male[0].values), width)
+    p2 = axis[1, 1].bar(ind, tuple(df_female[0].values), width, bottom = tuple(df_male[0].values))
+    axis[1, 1].set_ylabel('Contribution')
+    axis[1, 1].set_title('Contribution by the teams')
+    axis[1, 1].set_xticks(ind)
+    axis[1, 1].set_xticklabels(['Airhostess', 'Pilot', 'Steward'])
+    axis[1, 1].legend((p1[0], p2[0]), ('Male', 'Female'))
     plt.show()
 
 if __name__ == '__main__':
